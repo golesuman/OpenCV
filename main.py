d8f785661cf6cee0aab26b45b7f6d38196c064d8
Suman Gole <60792767+golesuman@users.noreply.github.com>
2021-03-08 18:10:57 +0545

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 import cv2 as cv
-im=imread("")
+vid=cv.VideoCapture(0)
+forcc=cv.VideoWriter_fourcc('X','V','I','D')
+out=cv.VideoWriter("suman.mp4",forcc,30.0,(640,480))
+
+while(vid.isOpened()):
+    ret,frame=vid.read()
+    if ret ==True:
+        print(vid.get(cv.CAP_PROP_FRAME_WIDTH))
+        print(vid.get(cv.CAP_PROP_FRAME_HEIGHT))
+        out.write(frame)
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        cv.imshow("frame",gray)
+        if cv.waitKey(1)& 0xFF==ord("q"):
+            break
+    else:
+        break
+vid.release()
+out.release()
+cv.destroyAllWindows()
