e011e4035d484d1723d6036b791feeb0faf6113a
golesuman <gole35830@gmail.com>
2022-07-14 16:48:58 +0545

    update

--- PyImageSearch/Thresholding/adaptivethresholding.py
+++ PyImageSearch/Thresholding/adaptivethresholding.py
@@ -10,14 +10,14 @@ image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred = cv2.GaussianBlur(image, (5, 5), 0)
 
 cv2.imshow("Original",image)
-thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_MEAN_C,
+thresh = cv2.adaptiveThreshold(blurred, 230, cv2.ADAPTIVE_THRESH_MEAN_C,
                                 cv2.THRESH_BINARY_INV, 11, 4)
-cv2.imwrite("meanthresh.jpg", thresh)
+# cv2.imwrite("meanthresh.jpg", thresh)
 cv2.imshow("Mean Thresh", thresh)
-thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+thresh = cv2.adaptiveThreshold(blurred, 230, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
                             cv2.THRESH_BINARY_INV, 15, 3)
 
-cv2.imwrite("gaussianthresh.jpg", thresh)
+# cv2.imwrite("gaussianthresh.jpg", thresh)
 cv2.imshow("Gaussian Thresh", thresh)
 
 
